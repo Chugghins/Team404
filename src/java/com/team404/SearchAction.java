6d1c1fd3ef79516bc27e74c78f1ae2c5448a1b51
Chugghins <chugg1@bellsouth.net>
2017-04-25 01:20:31 -0500

    Things and stuff

--- src/java/com/team404/SearchAction.java
+++ src/java/com/team404/SearchAction.java
@@ -5,10 +5,39 @@
  */
 package com.team404;
 
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
 /**
  *
  * @author Chugg1
  */
-public class SearchAction {
-    
+public class SearchAction extends org.apache.struts.action.Action{
+    private static final String SUCCESS = "success";
+
+    /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     * @throws java.lang.Exception
+     * @return
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception
+    {
+        SearchDAO DAO = new SearchDAO();
+        ArrayList<Movie> movies = new ArrayList<>();
+        movies = DAO.search();
+        request.setAttribute("movies", movies);
+        return mapping.findForward(SUCCESS);
+    }
 }
--- src/java/com/team404/SearchDAO.java
+++ src/java/com/team404/SearchDAO.java
@@ -16,33 +16,163 @@ import java.util.ArrayList;
  * @author Chugg1
  */
 public class SearchDAO {
-        public ArrayList<Movie> search(String title) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException
-    {
+
+    public ArrayList<Movie> search() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         DbConnectionUtil dbUtil = new DbConnectionUtil();
         Connection con = dbUtil.getConnection();
         ArrayList<Movie> movies = new ArrayList<>();
-        try
-        {
+
+        try {
             Statement st = con.createStatement();
-            ResultSet resultSet = st.executeQuery("Select * From film Where title = " + title + ";");
+            ResultSet resultSet = st.executeQuery("Select f.film_id, title, release_year, language_id, rental_duration, "
+                    + "rental_rate, replacement_cost, rating, first_name, last_name, name From Film as f JOIN film_category As fc "
+                    + "ON f.film_id = fc.film_id JOIN category As c ON fc.category_id = c.category_id "
+                    + "JOIN film_actor As fa ON f.film_id = fa.film_id JOIN actor As a "
+                    + "ON a.actor_id = fa.actor_id;");
 
-            while (resultSet.next())
-            {
-                String orderNumber = resultSet.getString("order_num");
-                String customerID = resultSet.getString("customer_id");
-                String productID = resultSet.getString("product_id");
-                String quantity = resultSet.getString("quantity");
-                String shippingCost = resultSet.getString("shipping_cost");
-                String salesDate = resultSet.getString("sales_date");
-                String shippingDate = resultSet.getString("shipping_date");
+            while (resultSet.next()) {
+                int film_id = resultSet.getInt("film_id");
+                String title = resultSet.getString("title");
+                int release_year = resultSet.getInt("release_year");
+                int language_id = resultSet.getInt("language_id");
+                int rental_duration = resultSet.getInt("rental_duration");
+                double rental_rate = resultSet.getDouble("rental_rate");
+                double replacement_cost = resultSet.getDouble("replacement_cost");
+                String rating = resultSet.getString("rating");
+                String actor = resultSet.getString("first_name") + " " + resultSet.getString("last_name");
+                String genre = resultSet.getString("name");
 
-                Movie m = new Movie();
+                Movie m = new Movie(film_id, title, release_year, language_id, rental_duration, rental_rate, replacement_cost, rating, actor, genre);
+                movies.add(m);
+
             }
-        } catch (SQLException ex)
-        {
+            resultSet.close();
+        } catch (SQLException ex) {
         }
+
         con.close();
-        return ;
+        return movies;
 
     }
+    //        if (genre.equals("") && actor.equals("")) {
+//            try {
+//                Statement st = con.createStatement();
+//                ResultSet resultSet = st.executeQuery("Select f.film_id, release_year, language_id, rental_duration, "
+//                        + "rental_rate, replacement_cost, rating, first_name, last_name, name From Film as f JOIN film_category As fc "
+//                        + "ON f.film_id = fc.film_id JOIN category As c ON fc.category_id = c.category_id "
+//                        + "JOIN film_actor As fa ON f.film_id = fa.film_id JOIN actor As a "
+//                        + "ON a.actor_id = fa.actor_id WHERE title LIKE '%" + title + "%';");
+//
+//                while (resultSet.next()) {
+//                    int film_id = resultSet.getInt("film_id");
+//                    title = resultSet.getString("title");
+//                    int release_year = resultSet.getInt("release_year");
+//                    int language_id = resultSet.getInt("language_id");
+//                    int rental_duration = resultSet.getInt("rental_duration");
+//                    double rental_rate = resultSet.getDouble("rental_rate");
+//                    double replacement_cost = resultSet.getDouble("replacement_cost");
+//                    String rating = resultSet.getString("rating");
+//                    actor = resultSet.getString("first_name") + " " + resultSet.getString("last_name");
+//                    genre = resultSet.getString("name");
+//
+//                    Movie m = new Movie(film_id, title, release_year, language_id, rental_duration, rental_rate, replacement_cost, rating, actor, genre);
+//                    movies.add(m);
+//                }
+//            } catch (SQLException ex) {
+//            }
+//        } else if (title.equals("") && actor.equals("")) {
+//            try {
+//                Statement st = con.createStatement();
+//                ResultSet resultSet = st.executeQuery("Select f.film_id, title, release_year, language_id, rental_duration, "
+//                        + "rental_rate, replacement_cost, rating, first_name, last_name From Film as f JOIN film_category As fc "
+//                        + "ON f.film_id = fc.film_id JOIN category As c ON fc.category_id = c.category_id "
+//                        + "JOIN film_actor As fa ON f.film_id = fa.film_id JOIN actor As a "
+//                        + "ON a.actor_id = fa.actor_id WHERE genre LIKE '%" + genre + "%';");
+//
+//                while (resultSet.next()) {
+//                    int film_id = resultSet.getInt("film_id");
+//                    title = resultSet.getString("title");
+//                    int release_year = resultSet.getInt("release_year");
+//                    int language_id = resultSet.getInt("language_id");
+//                    int rental_duration = resultSet.getInt("rental_duration");
+//                    double rental_rate = resultSet.getDouble("rental_rate");
+//                    double replacement_cost = resultSet.getDouble("replacement_cost");
+//                    String rating = resultSet.getString("rating");
+//                    actor = resultSet.getString("first_name") + " " + resultSet.getString("last_name");
+//                    genre = resultSet.getString("name");
+//
+//                    Movie m = new Movie(film_id, title, release_year, language_id, rental_duration, rental_rate, replacement_cost, rating, actor, genre);
+//                    movies.add(m);
+//                }
+//            } catch (SQLException ex) {
+//            }
+//        } else if (genre.equals("") && title.equals("")) {
+//            if (actor.contains(" ")) {
+//                String[] first_last = actor.split(" ");
+//                try {
+//                    Statement st = con.createStatement();
+//                    ResultSet resultSet = st.executeQuery("Select f.film_id, release_year, language_id, rental_duration, "
+//                            + "rental_rate, replacement_cost, rating, name From Film as f JOIN film_category As fc "
+//                            + "ON f.film_id = fc.film_id JOIN category As c ON fc.category_id = c.category_id "
+//                            + "JOIN film_actor As fa ON f.film_id = fa.film_id JOIN actor As a "
+//                            + "ON a.actor_id = fa.actor_id WHERE first_name LIKE '%" + first_last[0]
+//                            + "%' AND last_name LIKE '%" + first_last[1] + "%';");
+//
+//                    while (resultSet.next()) {
+//                        int film_id = resultSet.getInt("film_id");
+//                        int release_year = resultSet.getInt("release_year");
+//                        int language_id = resultSet.getInt("language_id");
+//                        int rental_duration = resultSet.getInt("rental_duration");
+//                        double rental_rate = resultSet.getDouble("rental_rate");
+//                        double replacement_cost = resultSet.getDouble("replacement_cost");
+//                        String rating = resultSet.getString("rating");
+//                        actor = resultSet.getString("first_name") + " " + resultSet.getString("last_name");
+//                        genre = resultSet.getString("name");
+//
+//                        Movie m = new Movie(film_id, title, release_year, language_id, rental_duration, rental_rate, replacement_cost, rating, actor, genre);
+//                        movies.add(m);
+//                    }
+//
+//                } catch (SQLException ex) {
+//                }
+//            } else {
+//
+//                try {
+//                    Statement st = con.createStatement();
+//                    ResultSet resultSet = st.executeQuery("Select f.film_id, release_year, language_id, rental_duration, "
+//                            + "rental_rate, replacement_cost, rating, name From Film as f JOIN film_category As fc "
+//                            + "ON f.film_id = fc.film_id JOIN category As c ON fc.category_id = c.category_id "
+//                            + "JOIN film_actor As fa ON f.film_id = fa.film_id JOIN actor As a "
+//                            + "ON a.actor_id = fa.actor_id WHERE first_name LIKE '%" + actor
+//                            + "%' OR last_name LIKE '%" + actor + "%';");
+//
+//                    while (resultSet.next()) {
+//                        int film_id = resultSet.getInt("film_id");
+//                        int release_year = resultSet.getInt("release_year");
+//                        int language_id = resultSet.getInt("language_id");
+//                        int rental_duration = resultSet.getInt("rental_duration");
+//                        double rental_rate = resultSet.getDouble("rental_rate");
+//                        double replacement_cost = resultSet.getDouble("replacement_cost");
+//                        String rating = resultSet.getString("rating");
+//                        actor = resultSet.getString("first_name") + " " + resultSet.getString("last_name");
+//                        genre = resultSet.getString("name");
+//
+//                        Movie m = new Movie(film_id, title, release_year, language_id, rental_duration, rental_rate, replacement_cost, rating, actor, genre);
+//                        movies.add(m);
+//                    }
+//
+//                } catch (SQLException ex) {
+//                }
+//            }
+//        } else if (genre.equals("")) {
+//
+//        } else if (actor.equals("")) {
+//
+//        } else if (actor.equals("")) {
+//
+//        }
+//        else
+//        {
+//            
+//        }
 }
