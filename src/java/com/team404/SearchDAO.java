a7abd40606d8be8f4cd952a0f86dff652946d2a0
Chugghins <chugg1@bellsouth.net>
2017-04-24 21:00:49 -0500

    Things and stuff

---
+++ src/java/com/team404/SearchDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.team404;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Chugg1
+ */
+public class SearchDAO {
+       
+}
